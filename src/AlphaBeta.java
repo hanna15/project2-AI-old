a73cf78125328e36dcabccad722318f2d9f1bd82
hanna15 <hanna15@ru.is>
2017-02-21 08:50:10 +0000

    fixing alpha-beta

--- src/AlphaBeta.java
+++ src/AlphaBeta.java
@@ -1,12 +1,14 @@
 package proj2;
 
+import java.util.ArrayList;
+
 public class AlphaBeta {
 	static final int MAX = Integer.MAX_VALUE;
 	static final int MIN = Integer.MIN_VALUE;
 	
 	
 	//svo depth og playclock, startclock etc
-	private int value;
+	//private int value;
 	private int envWidth;
 	private int envHeight;
 	private State currState;
@@ -17,23 +19,33 @@ public class AlphaBeta {
 		envWidth = w;
 		envHeight = h;
 		myRole = role;
-		value = maxValue(currState, MIN, MAX);
+		//value = 0;
+		//value = maxValue(currState, MIN, MAX);
 		//return the action in ACTIONS(state) with value v
 		//bestMove();
 	}
 	
-	Move bestMove() { //dummy fall
-		return new Move(new Position(0,0), new Position(0,0), true);
+	Move bestMove() { 
+		ArrayList<Move> moves = currState.getAllLegalMoves(envWidth, envHeight);
+		System.out.println("-----------size of legal moves: " + moves.size());
+		int max_val = MIN;
+		Move max_move = null;
+		for (Move m : currState.getAllLegalMoves(envWidth, envHeight)) {
+			int val = minValue(currState.successorState(m), MIN, MAX); //synist ekki skipta mali hvort byrjum a min eda max
+			//System.out.println("-----------val is : " + val);
+			if (val >= max_val) {
+				//System.out.println("------------in if----------------");
+				max_val = val;
+				max_move = m;
+			}
+		}
+		return max_move;
+		//System.out.println("---max move is--- : " + max_move);
 	}
-	//Move bestMove() {
-		//for (Move m : currState.getAllLegalMoves(envWidth, envHeight)) {
-			//if( m.getUtility() == value) { //return the action in ACTIONS(state) with value v
-			//	return m;                 //veit ekki alveg hvernig vid faum thetta utility ?
-			//}
-		//}
-	//}
+	
 
 	private int minValue(State s, int alpha_min, int beta_max) {
+		int value = MAX;
 		if(s.isGoalState(envHeight)) { //veit hann potto "who's turn it is ?"
 			 if (s.isWhite && myRole.equals("white") || !s.isWhite && myRole.equals("black")) { //if we are the winners
 				 return 100; 
@@ -43,7 +55,7 @@ public class AlphaBeta {
 			 }
 			 return 0; //??? when would we really return 0 ?
 		} 
-		value = MAX;
+		//value = MAX;
 		for (Move m : s.getAllLegalMoves(envWidth, envHeight)) {
 			value = Math.min(value, maxValue(s.successorState(m), alpha_min, beta_max));
 			if(value <= alpha_min) {
@@ -55,13 +67,17 @@ public class AlphaBeta {
 	}
 	
 	private int maxValue(State s, int alpha_min, int beta_max) {
-		if(s.isGoalState(envHeight)) {
-			 if (s.isWhite) { //return the utility of the state - hvernig er thad reiknad ? //rather: if we are the player
-				 return 100;  //lika hvenaer skilum vid 0 ?
+		int value = MIN;
+		if(s.isGoalState(envHeight)) { //veit hann potto "who's turn it is ?"
+			 if (s.isWhite && myRole.equals("white") || !s.isWhite && myRole.equals("black")) { //if we are the winners
+				 return 100; 
 			 }
-			 return -100;
-		}
-		value = MIN;
+			 else if(s.isWhite && myRole.equals("black") || !s.isWhite && myRole.equals("white")) { //if we are the loosers
+				 return -100;
+			 }
+			 return 0; //??? when would we really return 0 ?
+		} 
+		//value = MIN;
 		for (Move m : s.getAllLegalMoves(envWidth, envHeight)) {
 			value = Math.max(value, minValue(s.successorState(m), alpha_min, beta_max));
 			if(value >= beta_max) {
